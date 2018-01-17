b14eff2da459ce407b5164873bec843c48585e21
Brett Hobbs <brett.hobbs@gmail.com>
2018-01-17 09:53:22 -0800

    clean up

--- stories/index.js
+++ stories/index.js
@@ -2,11 +2,9 @@ import React from 'react'
 
 import { action } from '@storybook/addon-actions'
 
-import { storiesOf } from '@storybook/react'
 import { Toolbar } from '../src/toolbar'
+import { storiesOf } from '@storybook/react'
 
 storiesOf('Alignment Toolbar', module)
-  .add('Disabled', () => <Toolbar />)
-  .add('Left Aligned', () => <Toolbar alignment="left" onChange={action('onChange')} />)
-  .add('Center Aligned', () => <Toolbar alignment="center" onChange={action('onChange')} />)
-  .add('Right Aligned', () => <Toolbar alignment="right" onChange={action('onChange')} />)
+  .addWithJSX('Enabled', () => <Toolbar onChange={action('onChange')} />)
+  .addWithJSX('Disabled', () => <Toolbar />)
